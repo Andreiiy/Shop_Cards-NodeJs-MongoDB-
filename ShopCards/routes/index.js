e94701bf03a27b18a6908792b874bb15b682a7bb
Andrei <41228222+Andreiiy@users.noreply.github.com>
2020-02-23 23:22:50 +0200

    Created CSRF protection

--- ShopCards/routes/index.js
+++ ShopCards/routes/index.js
@@ -2,7 +2,10 @@
 var express = require('express');
 var router = express.Router();
 var Product = require('../models/product');
+var csrf = require('csurf');
 
+var csrfProtection = csrf();
+router.use(csrfProtection);
 /* GET home page. */
 router.get('/', function (req, res) {
     var products = Product.find(function (err, docs) {
@@ -14,6 +17,14 @@ router.get('/', function (req, res) {
         res.render('shop/index', { title: 'Internet Shop', products: productChunks });
     });
     
+});
+
+router.get('/user/signup', function (req, res, next) {
+    res.render('user/signup', { csrfToken: req.csrfToken() });
+});
+
+router.post('/user/signup', function (req, res, next) {
+    res.redirect('/');
 });
 
 module.exports = router;
